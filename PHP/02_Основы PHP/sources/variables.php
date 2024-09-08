06e9ad9cdf6c27a75f73462f94886363d5791874
demfireld <gamew5177@gmail.com>
2024-09-08 14:52:56 +0300

    vault backup: 2024-09-08 14:52:56

--- PHP/02_Основы PHP/sources/variables.php
+++ PHP/02_Основы PHP/sources/variables.php
@@ -1,3 +1,17 @@
 <?php
-$num;
+$num = 10;
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Переменные</title>
+</head>
+<body>
+    <?php
+    echo $num;
+    ?>
+</body>
+</html>
