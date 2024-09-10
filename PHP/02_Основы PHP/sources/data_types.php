4cad2f6d252ecb93f4261c505f0a6f292ad3da62
demfireld <gamew5177@gmail.com>
2024-09-10 18:02:29 +0300

    vault backup: 2024-09-10 18:02:29

--- PHP/02_Основы PHP/sources/data_types.php
+++ PHP/02_Основы PHP/sources/data_types.php
@@ -6,5 +6,8 @@ $num_10 = 28;
 $num_2 = 0b11100;
 $num_8 = 034;
 $num_16 = 0x1C;
-echo "num_10: $num_10<br />"
+echo "num_10: $num_10<br />";
+echo "num_2: $num_2<br />";
+echo "num_8: $num_8<br />";
+echo "num_16: $num_16<br />";
 ?>
