851daf0f2e7deacb625b2a81a4a296fb24677cc7
Nazari <gamew5177@gmail.com>
2024-09-14 13:15:07 +0300

    vault backup: 2024-09-14 13:15:07

---
+++ JS/Основы JS/01_variables_and_const/js/main.js
@@ -0,0 +1,16 @@
+var username;
+let username;
+
+// Правильные название переменных
+$commision
+someVariable
+product_Store
+income2
+myIncome_from_deposit
+
+// Некорректные названия переменных
+/*
+222lol
+@someVariable
+my%percent
+*/
