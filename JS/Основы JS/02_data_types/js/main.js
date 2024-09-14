ad70a145ce0a73aa299d65bfb0461a7f8f6c36f1
demfireld <gamew5177@gmail.com>
2024-09-15 00:14:40 +0300

    vault backup: 2024-09-15 00:14:40

--- JS/Основы JS/02_data_types/js/main.js
+++ JS/Основы JS/02_data_types/js/main.js
@@ -31,3 +31,46 @@ const company2 = "Бюро 'Рога и копыта'";
 console.log(company2);
 const company3 = 'Бюро "Рога и копыта"';
 console.log(company3);
+
+const text = "Hello METANIT.COM\nHello\tWorld";
+console.log(text);
+
+const user2 = "Tom";
+const text2 = `Name ${user2}`;
+console.log(text2);
+
+const user3 = "Tom";
+const age3 = 19;
+const isMarried3 = false;
+const text3 = `Name: ${user3}\nAge: ${age3}\nIsMarried: ${isMarried3}`;
+console.log(text3);
+
+const text4 =
+`Мы все учились понемногу
+Чему-нибудь и как-нибудь,
+Так воспитаньем, слава богу,
+У нас немудрено блеснуть.`;
+console.log(text4);
+
+let email;
+console.log(email);
+email = null;
+console.log(email);
+email = "tomme@mail.ru";
+console.log(email);
+email = undefined;
+console.log(email);
+
+const user4 = { name: "Tom", age: 19 };
+console.log(user4);
+
+let id;
+console.log(typeof id);
+id = 45;
+console.log(typeof id);
+id = 45n;
+console.log(typeof id);
+id = "45";
+console.log(typeof id);
+id = null;
+console.log(typeof id);
