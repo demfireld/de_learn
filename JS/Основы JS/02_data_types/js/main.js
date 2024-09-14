0b5de516ee949d5aea6f04e227447d2f6ca869e0
demfireld <gamew5177@gmail.com>
2024-09-14 23:52:03 +0300

    vault backup: 2024-09-14 23:52:03

---
+++ JS/Основы JS/02_data_types/js/main.js
@@ -0,0 +1,33 @@
+const x = 45;
+const y = 123.897;
+const z = -0.123;
+
+const num1 = 0b1011;
+const num2 = 0o11;
+const num3 = 0x1A;
+
+const num4 = 1234567;
+const num5 = 123_4567;
+
+let num = 9007199254740991;
+console.log(num);
+console.log(num + 1);
+console.log(num + 2);
+
+const isAlive = true;
+const isDead = false;
+
+const user = "Tom";
+const company = 'Microsoft';
+const language = 'JavaScript';
+
+console.log(user);
+console.log(company);
+console.log(language);
+
+const company1 = "Бюро \"Рога и копыта\"";
+console.log(company1);
+const company2 = "Бюро 'Рога и копыта'";
+console.log(company2);
+const company3 = 'Бюро "Рога и копыта"';
+console.log(company3);
